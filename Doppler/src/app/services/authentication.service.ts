58addc5e6e3ebb5c2fca2b99bb418a490888466c
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-03-13 10:43:20 +0200

    Updated Doppler Angular

---
+++ Doppler/src/app/services/authentication.service.ts
@@ -0,0 +1,7 @@
+import {HttpClient} from '@angular/common/http';
+
+export class AuthenticationServoce {
+    constructor(private http: HttpClient){
+
+    }
+}
---
+++ Doppler/src/app/services/dist/authentication.service.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.AuthenticationServoce = void 0;
+var AuthenticationServoce = /** @class */ (function () {
+    function AuthenticationServoce(http) {
+        this.http = http;
+    }
+    return AuthenticationServoce;
+}());
+exports.AuthenticationServoce = AuthenticationServoce;
