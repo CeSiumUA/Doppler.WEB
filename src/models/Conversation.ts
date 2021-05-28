2ecf285da26e2c2deec0c1ceef40ffa30ee30278
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-05-28 09:55:16 +0300

    fixed problem with icon url

--- src/models/Conversation.ts
+++ src/models/Conversation.ts
@@ -6,14 +6,10 @@ export interface Conversation{
     id?: string;
     name?: string;
     description?: string;
-    iconUrl?: string;
+    iconUrl: string;
 }
 
 export interface Dialogue extends Conversation{
-    id?: string | undefined;
-    name?: string | undefined;
-    description?: string | undefined;
-    iconUrl?: string;
     firstUser?: ConversationMember;
     secondUser?: ConversationMember;
 }
